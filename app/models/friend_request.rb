b643c1bd6c8b4f8b34ead9f7f2f0d0141da13dfa
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-05-10 16:13:21 +0200

    Delete friend_request.rb

--- app/models/friend_request.rb
+++
@@ -1,7 +0,0 @@
-class FriendRequest < ApplicationRecord
-  validates :sender, presence: true
-  validates :receiver, presence: true
-
-  belongs_to :sender, class_name: "User"
-  belongs_to :receiver, class_name: "User"
-end
