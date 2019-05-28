17f38241d1ae57d812f08bdb37170194ffa56a62
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-05-28 17:09:54 +0200

    removing comments and fixed posts page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,7 @@ class CommentsController < ApplicationController
     end
 
     def destroy
+        @comment = Comment.find(params[:id])
         @comment.destroy
         redirect_back(fallback_location: root_path)
       end
