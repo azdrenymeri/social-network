4f17fe9f3dc01fcd0ee1e120dc09c6a707deb145
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-05-28 16:49:13 +0200

    create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+    before_action :logged_in_user
 
     def index
         @comments = Comment.all
@@ -18,17 +18,23 @@ class CommentsController < ApplicationController
         @comment = Comment.find(params[:id])
     end
 
+    def destroy
+        @comment.destroy
+        redirect_back(fallback_location: root_path)
+      end
+
     def create
-        @comment = Comment.new
-        @comment.content = params[:content]
-        @comment.post_id = Post.find(params[:post_id]).id
-        @comment.save
-        redirect_back(fallback_location: root_path) 
+        @comment = current_user.comments.new(comment_params)
+        if @comment.save
+          redirect_back(fallback_location: root_path)
+        else
+          redirect_back(fallback_location: root_path)
+        end
       end
 
     private
     def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :post_id)
     end
 
     
