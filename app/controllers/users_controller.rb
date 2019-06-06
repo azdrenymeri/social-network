08e93bce635f6bb9597999bdbf9b639802b0a171
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-06-06 11:19:14 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,7 @@ class UsersController < ApplicationController
         @users = User.people_you_might_know(current_user)
         @strangers = User.all - User.people_you_might_know(current_user)
         @z=User.all
-        puts "fcking shit"
-        @users.each do |user| 
-            puts user.email
-        end
-        puts "fcking shit"
-        @strangers.each do |user| 
-            puts user.email
-        end
-        puts "fcking shit"
-        @z.each do |user| 
-            puts user.email
-        end
+        
     end
 
     def show
