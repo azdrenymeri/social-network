ca6356ec1f21e6e208b70d30c4c9f63116ab4596
azdren <azdreny@gmail.com>
2019-06-13 14:44:20 +0200

    navigation bar design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def logged_in_user
-    unless user_signed_in?
-      flash[:warning] = "Please login."
-      redirect_to root_path
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root to:'feeds#index'
+  root to:'posts#index'
 
   devise_for :users,path_names:{sign_in:"login",sign_out:"logout"}, 
   controllers: { sessions: 'users/sessions',omniauth_callbacks: "users/omniauth_callbacks"}
