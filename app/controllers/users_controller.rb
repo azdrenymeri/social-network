4d31c14967873560a0e2b8abdbb86fa4c6b8e64d
azdren <azdreny@gmail.com>
2019-06-17 13:23:01 +0200

    user now can add his name and bio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,38 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.find(params[:id])
-        if params[:user][:picture].nil?
+        byebug
+        if  user_params[:id].blank? or
+            user_params[:name].blank? or 
+            user_params[:bio].blank? or 
+            user_params[:picture].blank?
+
+            flash[:danger] = "There was an error please check your form and re-submit it again"
             render "edit"
         else
+            @user = User.find(user_params[:id])
             @user.picture.purge
-            @user.picture.attach(params[:user][:picture])
-            redirect_to root_path
+            @user.picture.attach(user_params[:picture])
+            @user.name = user_params[:name]
+            @user.bio = user_params[:bio]
+
+            if @user.save
+                flash[:success] = "Your profile was set successfuly"
+                redirect_to user_path(@user)
+            else
+                flash[:danger] = "Something went wrong we couln't set your data"
+                render "edit" 
+            end
         end
     end
 
     def show
         @user = User.find(params[:id])
+        
     end
 
     private
     def user_params
-        params.require(:user).permit(:id, :picture)
+        params.require(:user).permit(:id, :name, :bio, :picture)
     end
 end
